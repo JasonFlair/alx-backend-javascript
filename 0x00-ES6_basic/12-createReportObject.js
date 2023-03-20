eb9c0c781280c561c530fe780ea97d7d9bf5ffb2
Jason Flair <emelieobumse100@gmail.com>
2023-03-20 19:42:23 +0100

    almost done

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,6 @@
+import returnHowManyArguments from "./4-rest-parameter"
+
+export default function createReportObject(employeesList) {
+    let everyemployeedetail = {allEmployees: employeesList};
+    return everyemployeedetail;
+}
