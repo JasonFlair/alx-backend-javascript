83c8abbe16f4da960efb7ffd8f9f4e9858be5150
Chukwuemelie Obumse <69307904+JasonFlair@users.noreply.github.com>
2023-03-20 20:01:39 +0100

    Update 12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,6 +1,10 @@
-import returnHowManyArguments from "./4-rest-parameter"
-
 export default function createReportObject(employeesList) {
-    let everyemployeedetail = {allEmployees: employeesList};
-    return everyemployeedetail;
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
 }
