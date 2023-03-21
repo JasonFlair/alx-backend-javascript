ac8c976f3909071f19a315246503bae2d13b8cc6
Jason Flair <emelieobumse100@gmail.com>
2023-03-21 06:32:44 +0100

    all done

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
   for (const idx of array) {
     let value = idx;
     value = appendString + value;
+    newArray.push(value);
   }
 
-  return array;
+  return newArray;
 }
--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,23 +1,11 @@
-<<<<<<< HEAD
-export default function createReportObject(employeesList) {
-  return {
-    allEmployees: employeesList,
-    getNumberOfDepartments() {
-      const listOfDepts = Object.keys(employeesList);
-      return listOfDepts.length;
-    },
-
-  };
-}
-=======
 export default function createReportObject(employeesList) {
   return {
     allEmployees: {
       ...employeesList,
     },
-    getNumberOfDepartments(employeesList) {
-      return Object.keys(employeesList).length;
+    getNumberOfDepartments() {
+      const listOfDepts = Object.keys(employeesList);
+      return listOfDepts.length;
     },
   };
 }
->>>>>>> 6882ec3407c4b1b848358386d600b72751b373f8
