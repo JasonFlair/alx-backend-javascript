e967edd4686708e2ecd135379e9ef297f36eec9f
Jason Flair <emelieobumse100@gmail.com>
2023-03-22 10:56:12 +0100

    should work

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,15 @@ export default function handleProfileSignup(firstName, lastName, filename) {
   const newUser = signUpUser(firstName, lastName);
   const rejectedPhoto = uploadPhoto(filename);
   return Promise.allSettled([newUser, rejectedPhoto])
-    .then((results) => results);
+    .then((results) => {
+      const statusArray = [];
+      results.forEach((result) => {
+        if (result.status === 'fulfilled') {
+          statusArray.push({ status: result.status, value: result.value });
+        } else {
+          statusArray.push({ status: result.status, value: `${result.reason}` });
+        }
+      });
+      return statusArray;
+    });
 }
