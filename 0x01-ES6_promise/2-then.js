0612c6439f603a2d082661a85cdb3e147110c971
Jason Flair <emelieobumse100@gmail.com>
2023-03-22 02:29:32 +0100

    promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,8 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(() => {
     if (promise) {
-      console.log('Got a response from the API');
       return ({ status: 200, body: 'Success' });
     }
-    return (Error());
-  });
+  }).catch(() => Error())
+    .finally(() => { console.log('Got a response from the API'); });;
 }
