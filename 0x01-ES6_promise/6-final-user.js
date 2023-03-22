ef294e76f3606bba481479c126f28012c050bdea
Jason Flair <emelieobumse100@gmail.com>
2023-03-22 04:18:15 +0100

    tuff

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,6 +4,6 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, filename) {
   const newUser = signUpUser(firstName, lastName);
   const rejectedPhoto = uploadPhoto(filename);
-  const finalPromise = Promise.all([newUser, rejectedPhoto]).catch(() => {});
-  return (finalPromise);
+  return Promise.allSettled([newUser, rejectedPhoto])
+    .then((results) => results);
 }
