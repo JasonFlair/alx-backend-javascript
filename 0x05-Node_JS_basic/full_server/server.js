deb472ca25e5f9b2e41592d33605c81657fb8262
Chukwuemelie Obumse <69307904+JasonFlair@users.noreply.github.com>
2023-05-26 21:15:37 +0100

    Update server.js

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -4,9 +4,8 @@ const allRoutes = require('./routes/index');
 const app = express();
 const port = 1245;
 
+// sets all the routes created in routes/index
 app.use('/', allRoutes);
-app.use('/students', allRoutes);
-app.use('/students/:major', allRoutes);
 
 app.listen(port);
 module.exports = app;
